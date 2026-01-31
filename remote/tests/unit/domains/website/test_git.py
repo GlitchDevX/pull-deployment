f396aaafe555d5ae54ce763b7554f5d6a3768ac0
GlitchDevX <jonas.ringeis@gmail.com>
2026-01-31 18:41:10 +0100

    feat: add test case for pull with access token & replace deployment_stub fixture with build_stub method

--- remote/tests/unit/domains/website/test_git.py
+++ remote/tests/unit/domains/website/test_git.py
@@ -28,32 +28,46 @@ def mock_rmtree(mocker: MockerFixture):
 def mock_copytree(mocker: MockerFixture):
     return mocker.patch(*ref(shutil.copytree))
 
-@pytest.fixture
-def deployment_stub():
+def build_deployment_stub(access_token: str | None = "access_token"):
     return Deployment(
         id="test-deployment-id",
         secret="ultra-secret-test-secret",
         remote="github.com/torvalds/linux",
-        target_dir="tests/linux"
+        target_dir="tests/linux",
+        access_token=access_token
     )
 
 
-def test_should_pull_temp_branch(mocker: MockerFixture, deployment_stub, mock_subprocess, mock_tmp_dir, mock_copytree, mock_rmtree):
+def test_should_pull_temp_branch_without_access_token(mocker: MockerFixture, mock_subprocess, mock_tmp_dir, mock_copytree, mock_rmtree):
     mock_subprocess.return_value = mocker.Mock(returncode=0)
     given_branch_name = "example-deployment-1234"
+    given_deployment = build_deployment_stub(access_token=None)
 
-    pull_temp_branch(given_branch_name, deployment_stub)
+    pull_temp_branch(given_branch_name, given_deployment)
 
     expected_args = ["git", "clone", "-b", given_branch_name, "--depth", "1", "https://github.com/torvalds/linux", "."]
     mock_subprocess.assert_called_once_with(expected_args, cwd=TMP_TEST_DIR)
     mock_rmtree.assert_called_once_with(TMP_TEST_DIR + "/.git", ignore_errors=True)
-    mock_copytree.assert_called_once_with(TMP_TEST_DIR, deployment_stub.target_dir, dirs_exist_ok=True)
+    mock_copytree.assert_called_once_with(TMP_TEST_DIR, given_deployment.target_dir, dirs_exist_ok=True)
 
-def test_should_handle_non_existent_branch(mocker: MockerFixture, deployment_stub, mock_subprocess, mock_tmp_dir, mock_copytree, mock_rmtree):
+def test_should_pull_temp_branch_with_access_token(mocker: MockerFixture, mock_subprocess, mock_tmp_dir, mock_copytree, mock_rmtree):
+    mock_subprocess.return_value = mocker.Mock(returncode=0)
+    given_branch_name = "example-deployment-1234"
+    given_deployment = build_deployment_stub(access_token="access_token")
+
+    pull_temp_branch(given_branch_name, given_deployment)
+
+    expected_args = ["git", "clone", "-b", given_branch_name, "--depth", "1", "https://access_token@github.com/torvalds/linux", "."]
+    mock_subprocess.assert_called_once_with(expected_args, cwd=TMP_TEST_DIR)
+    mock_rmtree.assert_called_once_with(TMP_TEST_DIR + "/.git", ignore_errors=True)
+    mock_copytree.assert_called_once_with(TMP_TEST_DIR, given_deployment.target_dir, dirs_exist_ok=True)
+
+def test_should_handle_non_existent_branch(mocker: MockerFixture, mock_subprocess, mock_tmp_dir, mock_copytree, mock_rmtree):
     mock_subprocess.return_value = mocker.Mock(returncode=1)
     given_branch_name = "non-existent-deployment-2345"
+    given_deployment = build_deployment_stub(access_token=None)
 
-    pull_temp_branch(given_branch_name, deployment_stub)
+    pull_temp_branch(given_branch_name, given_deployment)
 
     expected_args = ["git", "clone", "-b", given_branch_name, "--depth", "1", "https://github.com/torvalds/linux", "."]
     mock_subprocess.assert_called_once_with(expected_args, cwd=TMP_TEST_DIR)
