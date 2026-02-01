e837480840a0a471ac2621d38d4fa7232ce1becf
GlitchDevX <jonas.ringeis@gmail.com>
2026-02-01 23:53:51 +0100

    feat: remove access token from website deployment body as the token from ci is overprivileged

--- remote/domains/website/models.py
+++ remote/domains/website/models.py
@@ -7,9 +7,6 @@ class WebsiteBody(BaseBody):
   deployment_id: str = Field(description="Identifier of deployment to trigger")
   deployment_secret: str = Field(description="Secret of deployment to trigger") # maybe move to header
   branch_name: str = Field(description="Name of the temporary branch the content was pushed to")
-  
-  # thought here is to use the gh token in the action, but the downside would be an overpriviliged token because of required write permission
-  access_token: str | None = Field(default=None, description="Access token to have pull access on the temporary branch")
 
 class WebsiteResponse(BaseBody):
   result: str
