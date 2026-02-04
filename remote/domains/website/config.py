44330c98f2b54982d48021bf5fcf7cae55eb4759
Jonas Ringeis <private@glitchdev.me>
2026-02-04 13:14:49 +0100

    feat: add config loading cache

--- remote/domains/website/config.py
+++ remote/domains/website/config.py
@@ -1,15 +1,25 @@
-from fastapi.exceptions import HTTPException
-from domains.shared.logger import logger
-from pydantic import ValidationError
-from domains.website.models import AppConfig
 import yaml
+from functools import lru_cache
+
+from frozendict import frozendict
+from frozendict.cool import deepfreeze
+from pydantic import ValidationError
+from fastapi.exceptions import HTTPException
+
+from domains.shared.logger import logger
+from domains.website.models import AppConfig
 
 
 def load_config(path: str = "config/config.yml") -> AppConfig:
     parsed_yaml = _load_yaml(path)
+    return _parse_config(path, parsed_yaml)
+
+@lru_cache
+def _parse_config(path: str, content: frozendict) -> AppConfig:
     try:
-        return AppConfig(**parsed_yaml)
-    except ValidationError as err:
+        return AppConfig(**content)
+    except (ValidationError, TypeError) as err:
+        _load_yaml.cache_clear()
         logger.error(f"Config at '{path}' is invalid.\n{str(err)}")
         raise HTTPException(
             status_code=500,
@@ -17,6 +27,8 @@ def load_config(path: str = "config/config.yml") -> AppConfig:
         )
 
 
-def _load_yaml(path: str):
+@lru_cache
+def _load_yaml(path: str) -> frozendict:
+    print("called fs read")
     with open(path) as file:
-        return yaml.safe_load(file)
+        return deepfreeze(yaml.safe_load(file))
